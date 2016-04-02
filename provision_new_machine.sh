56197f4c7bbaf5df4404e66e1e24a37cea0e1619
hly189 <lythaihoa2311992@gmail.com>
2016-04-02 01:07:36 -0700

    update ec2 and modifying provision new machine

--- provision_new_machine.sh
+++ provision_new_machine.sh
@@ -16,18 +16,25 @@ SUBNET=$3
 AMI=$4
 TYPE=$5
 
+#set -x
 # Running playbook to create ec2 instace
-cd /etc/ansible
-ansible-playbook playbooks/ec2.yml -e "MACHINE_NAME=$NAME REGION=$REGION SUBNET_ID=$SUBNET AMI=$AMI TYPE=$INSTANCE_TYPE"
+echo "Provisioning a new EC2 instance based input information"
+date 
+
+#cd /etc/ansible
+ID=$(ansible-playbook -i hosts ec2.yml -e "MACHINE_NAME=$NAME REGION=$REGION SUBNET_ID=$SUBNET AMI=$AMI INSTANCE_TYPE=$TYPE" | grep "i-")
+
+echo "ID": $ID
+
 
 #Update host file
-cd /etc/ansible
-ansible-playbook playbooks/updt_hosts_file_pb.yml
+#cd /etc/ansible
+#ansible-playbook playbooks/updt_hosts_file_pb.yml
 
 #Update inventory file
-cd /etc/ansible
-ansible-playbook playbooks/updt_inventory_file_pb.yml 
+#cd /etc/ansible
+#ansible-playbook playbooks/updt_inventory_file_pb.yml 
 
 # Update base for the instance
-ansible-playbook /playbooks/base_pb.yml
+#ansible-playbook /playbooks/base_pb.yml
 
