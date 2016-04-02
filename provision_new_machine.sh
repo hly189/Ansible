da1db0deaf33e58cec445b0097c5c7e3f58ba196
Hoa Ly <lythaihoa2311992@gmail.com>
2016-04-02 12:43:17 -0700

    Automate most of the part

--- provision_new_machine.sh
+++ provision_new_machine.sh
@@ -22,19 +22,26 @@ echo "Provisioning a new EC2 instance based input information"
 date 
 
 #cd /etc/ansible
-ID=$(ansible-playbook -i hosts ec2.yml -e "MACHINE_NAME=$NAME REGION=$REGION SUBNET_ID=$SUBNET AMI=$AMI INSTANCE_TYPE=$TYPE" | grep "i-")
+ansible-playbook -i hosts ec2.yml -e "MACHINE_NAME=$NAME REGION=$REGION SUBNET_ID=$SUBNET AMI=$AMI INSTANCE_TYPE=$TYPE"
 
+ID=$(ec2-describe-tags --region $REGION | grep $NAME | awk '/instance/{ print $3}')
+IP=$(aws ec2 describe-instances --region $REGION --instance-ids $ID | awk ' /"PrivateIpAddress"/{gsub(/[",]+/, "", $2);print $2}' | sed -n '1p')
+
+echo "IP": $IP
 echo "ID": $ID
 
 
 #Update host file
-#cd /etc/ansible
-#ansible-playbook playbooks/updt_hosts_file_pb.yml
+cd /etc/ansible
+ansible-playbook playbooks/updt_hosts_file_pb.yml -e "ENV=qa IP=$IP SRVR_NAME=$NAME"
 
 #Update inventory file
-#cd /etc/ansible
-#ansible-playbook playbooks/updt_inventory_file_pb.yml 
+cd /etc/ansible
+ansible-playbook playbooks/updt_inventory_file_pb.yml -e "MCN=$NAME"
 
+echo "Sleep 2 minutes before it can provision base role"
+date 
+sleep 120
 # Update base for the instance
-#ansible-playbook /playbooks/base_pb.yml
+ansible-playbook /playbooks/base_pb.yml -e "target=$NAME"
 
