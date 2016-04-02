7b24035670cc0df9d2a7134efc08d3a667fb311f
hly189 <lythaihoa2311992@gmail.com>
2016-04-01 23:04:18 -0700

    update delete instace for the Ansible folder

--- ec2_delete_instance.sh
+++ ec2_delete_instance.sh
@@ -16,4 +16,31 @@ aws ec2 describe-instance-status --region $REGION | grep -i "InstanceId" | cut -
 echo Which instance do you want to termninate?
 read INSTANCE_ID
 
+
+while read line
+do
+    vol[ $i ]="$line"
+    echo The volume is ${vol[$i]}
+    i=$[$i+1]
+done < <(aws ec2 describe-instances --region us-west-2 --instance $INSTANCE_ID | awk '/VolumeId/{gsub(/[",]+/, "", $2); print $2}')
+
+
 aws ec2 terminate-instances --region $REGION --instance-ids $INSTANCE_ID
+
+date
+echo Sleeping 2 minutes before deleting the volumes 
+
+sleep 120
+
+for index in "${!vol[@]}"
+do
+        length=$index
+done
+
+i=0
+while [ $i -le $length ]
+do 
+	aws ec2 delete-volume --region $REGION --volume-id ${vol[$i]} || true
+	echo Deleting ${vol[$i]}
+	 i=$[$i+1]
+done 
